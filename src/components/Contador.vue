492db134bacb1636fdf6777e9d0518d73ebd782d
Bruno Mentasti <bmentasti@hotmail.com>
2021-11-12 10:43:05 -0300

    Mi primer commit del desafio

---
+++ src/components/Contador.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="new-component">
+    <h1>A New Component</h1>
+    <p>This is a text inside the NewComponent.</p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Contador',
+}
+</script>
+<style>
+/* Your CSS goes here */
+</style>

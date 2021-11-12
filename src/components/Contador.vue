60903733efdaf03749d7eca3c7d36a7a5b55d278
Bruno Mentasti <bmentasti@hotmail.com>
2021-11-12 11:29:04 -0300

    Se agregaron styles

--- src/components/Contador.vue
+++ src/components/Contador.vue
@@ -4,6 +4,7 @@
     <h2>{{contadorPadre}}</h2>
     <button v-on:click="aumenta()">+</button>
     <button v-on:click="resta()">-</button>
+    <p><a href="https://www.linkedin.com/in/brunomentasti/">Bruno Mentasti</a> - Cómision 23345 - Vue.js </p>
   </div>
 </template>
 <script>
@@ -24,6 +25,42 @@ export default {
   }
 }
 </script>
-<style>
+<style scoped>
+h1 {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-size: 24px;
+  text-transform: uppercase;
+}
+h2 {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-size: 34px;
+  text-transform: uppercase;
+}
+p {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-size: 12px;
+}
+a {
+  text-decoration: none;
+  color:#2c3e50;
+}
+button {
+  background-color: limegreen;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+  -webkit-transition-duration: 0.4s;
+  transition-duration: 0.4s;
+}
+button:hover {
+  box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24), 0 17px 50px 0 rgba(0,0,0,0.19);
+}
+
 
 </style>
